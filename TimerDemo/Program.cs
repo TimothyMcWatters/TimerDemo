5bda183b571d4d29ff274144a05ebc8d10712e89
[TimothyMcWatters] <timmch2o@gmail.com>
2018-03-18 22:12:33 -0500

    Ready For User Testing

--- TimerDemo/Program.cs
+++ TimerDemo/Program.cs
@@ -10,6 +10,22 @@ namespace TimerDemo
     {
         static void Main(string[] args)
         {
+            DataBaseOperations.ConnectToDB();
+            TimerTestTable TTT = new TimerTestTable();
+
+            /*
+            List<String> list = new List<String>();
+            list.Add("Q3");
+            list.Add("A3");
+            list.Add("QType3");
+            TTT.InsertRowIntoTable(TTT.TableName, list);
+            */
+
+            Timer timer = new Timer();
+            timer.TimedMethod(TTT);
+
+
+            Console.ReadKey();
         }
     }
 }
